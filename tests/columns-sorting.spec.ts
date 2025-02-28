0def88ed5144be157ba7486c05b4eb76abfe9608
Brendon <elkins@webscope.io>
2025-02-28 10:23:15 +0100

    added test for column sorting

---
+++ tests/columns-sorting.spec.ts
@@ -0,0 +1,76 @@
+import { test, expect } from "@playwright/test";
+import { login } from "./commands.page";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const username = process.env.TEST_USERNAME || "";
+
+test.beforeEach(async ({ page }) => {
+  await login({ page });
+});
+
+test("check columns and sorting in user settings", async ({ page }) => {
+  await page.getByRole("button", { name: "Vehicles" }).click();
+  await expect(
+    page.locator("om-card-body").filter({ hasText: "All Vehicles (371)Map" })
+  ).toBeVisible();
+  await page.getByText("Ξ").click();
+  await expect(
+    page.getByText("Vehicle", { exact: true }).first()
+  ).toBeVisible();
+  await page.getByText("Ξ").click();
+  await page.getByText("Ξ").click();
+  await expect(
+    page.getByRole("columnheader", { name: "Vehicle" })
+  ).toBeVisible();
+  await expect(
+    page.getByRole("columnheader", { name: "Last active" })
+  ).toBeVisible();
+  await expect(page.getByRole("columnheader", { name: "Name" })).toBeVisible();
+  await expect(page.getByRole("columnheader", { name: "Speed" })).toBeVisible();
+  await expect(
+    page.getByRole("columnheader", { name: "Mileage" })
+  ).toBeVisible();
+  await page
+    .getByRole("columnheader", { name: "Last active" })
+    .getByRole("img")
+    .click();
+  await page.waitForTimeout(5000);
+  await page.goto("/");
+  await page.getByRole("button", { name: "Vehicles" }).click();
+  await expect(page.locator("i.icon-bus-any").first()).toBeVisible();
+
+  const columnValues = await page
+    .locator("table tbody tr td:nth-child(2)")
+    .allTextContents();
+
+  console.log("Raw column values:", columnValues);
+
+  const parseDate = (dateStr) => {
+    return new Date(
+      dateStr
+        .trim()
+        .replace(
+          /(\d{2})\.(\d{2})\.(\d{4}) (\d{2}):(\d{2}):(\d{2})/,
+          "$3-$2-$1T$4:$5:$6"
+        )
+    );
+  };
+
+  const dateValues = columnValues.map(parseDate);
+
+  console.log("Parsed Dates:", dateValues);
+
+  const isSortedAsc = dateValues.every(
+    (val, i, arr) => i === 0 || val >= arr[i - 1]
+  );
+  const isSortedDesc = dateValues.every(
+    (val, i, arr) => i === 0 || val <= arr[i - 1]
+  );
+  console.log(
+    isSortedAsc
+      ? "Column is sorted in ascending order ⬆️"
+      : "Column is sorted in descending order ⬇️"
+  );
+});
